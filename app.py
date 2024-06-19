38c58dbf463238ce9e85ad28ba1dad4db834ad8e
Xrayya <nur.alkhiyar@gmail.com>
2024-06-19 22:21:10 +0700

    refactor(app): wrap main into function

--- app.py
+++ app.py
@@ -49,6 +49,11 @@ api.add_resource(Test, '/')
 api.add_resource(GetRecommendation, '/recommendation')
 api.add_resource(ClaculateExp, '/exp')
 
-if __name__ == "__main__":
+
+def main():
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+# if __name__ == '__main__':
+    # main();
