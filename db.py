dd6b0a51bc3ef5bbdff34d337068f308fe2bc7ff
Xrayya <nur.alkhiyar@gmail.com>
2024-06-19 22:21:22 +0700

    feat(db): use environtment var

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 import mysql.connector
 import pandas as pd
+import os
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="rahasia",
-    database="qflare"
+    host=os.environ.get('MYSQL_DB_HOST'),
+    user=os.environ.get('MYSQL_DB_USER'),
+    passwd=os.environ.get('MYSQL_DB_PASSWORD'),
+    database=os.environ.get('MYSQL_DB_NAME')
 )
 
 
